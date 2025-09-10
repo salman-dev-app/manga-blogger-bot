9a967916eb0bdede43b0902c12864f8acd7da328
MD SALMAN ❤️‍🩹 <mdsalmanhelp0@gmail.com>
2025-09-10 12:56:15 +0600

    Update database.py

--- database.py
+++ database.py
@@ -7,17 +7,14 @@ db = client.get_database("MangaBotDB")
 state_collection = db.get_collection("bot_state")
 
 def get_state():
-    """ডেটাবেস থেকে বটের বর্তমান অবস্থা লোড করে"""
     state = state_collection.find_one({"_id": "global_state"})
     if not state:
-        # যদি ডেটাবেসে কোনো ডেটা না থাকে, তাহলে একটি খালি কাঠামো তৈরি করে
         return {"main_posts_created": [], "chapters_posted": {}}
     return state
 
 def save_state(data):
-    """বটের বর্তমান অবস্থাকে ডেটাবেসে সেভ করে"""
     state_collection.update_one(
         {"_id": "global_state"},
         {"$set": data},
-        upsert=True # যদি কোনো ডেটা না থাকে, তাহলে নতুন করে তৈরি করবে
+        upsert=True
     )
